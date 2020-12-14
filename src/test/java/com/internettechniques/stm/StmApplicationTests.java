2cd9ce680ca297f10a619786cac96021fa0f5108
Xabier Aresti <xabier.aresti@gmail.com>
2020-12-14 16:01:58 +0100

    Initial commit

---
+++ src/test/java/com/internettechniques/stm/StmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internettechniques.stm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
