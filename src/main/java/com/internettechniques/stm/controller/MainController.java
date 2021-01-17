dcbc24e06d5b0c4df66a935517383431d9ca2e0a
Xabier Aresti <xabier.aresti@gmail.com>
2021-01-17 21:23:56 +0100

    LAB2 V7

--- src/main/java/com/internettechniques/stm/controller/MainController.java
+++ src/main/java/com/internettechniques/stm/controller/MainController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class MainController {
     private final UserService userService;
     private final TaskService taskService;
